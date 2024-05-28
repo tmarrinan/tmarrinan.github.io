212e1513b24bec5f7714255d1014ab669882ce73
Thomas Marrinan <thomas.j.marrinan@gmail.com>
2024-05-28 16:34:12 -0500

    finish updates to navigation

--- src/Navigation.vue
+++ src/Navigation.vue
@@ -3,24 +3,40 @@ import { ref } from 'vue';
 const BASE_URL = import.meta.env.BASE_URL || '/';
 
 let job_title = ref('Associate Professor');
-let department = ref('Computer and Information Sciences');
-let employer = ref('University of St. Thomas');
 let email = ref('tmarrinan@stthomas.edu');
 </script>
 
 <template>
     <div id="tm-top-bar">
-        <div class="tm-menu" data-toggle="tm-mobile-menu">
-            <img class="tm-menu-icon" :src="BASE_URL + 'images/menu.png'" alt="menu open icon" />
+        <div id="tm-mobile-top-bar" class="hide-for-large">
+            <div id="tm-menu" data-toggle="tm-mobile-menu">
+                <img class="tm-menu-icon" :src="BASE_URL + 'images/menu.png'" alt="menu open icon" />
+            </div>
+            <div id="tm-info">
+                <p class="tm-info-text"><span class="tm-bold">Tommy Marrinan, PhD</span> | {{ email }}</p>
+                <p class="tm-info-text">{{ job_title }} &ndash; Computer Science</p>
+            </div>
         </div>
-        <div id="tm-info">
-            <p class="tm-info-text"><span class="tm-bold">Tommy Marrinan, PhD</span> | {{ employer }} | {{ email }}</p>
-            <p class="tm-info-text">{{ job_title }} &ndash; {{ department }}</p>
+        <div id="tm-desktop-top-bar" class="show-for-large">
+            <div id="tm-info">
+                <p class="tm-info-text"><span class="tm-bold">Tommy Marrinan, PhD</span> | {{ email }}</p>
+                <p class="tm-info-text">{{ job_title }} &ndash; Computer Science</p>
+            </div>
+            <div id="tm-navlinks">
+                <ul class="horizontal menu">
+                    <li><a :href="BASE_URL + 'index.html'" class="tm-nav-link">Home</a></li>
+                    <li><a :href="BASE_URL + 'teaching.html'" class="tm-nav-link">Teaching</a></li>
+                    <li><a :href="BASE_URL + 'research.html'" class="tm-nav-link">Research</a></li>
+                    <li><a :href="BASE_URL + 'publications.html'" class="tm-nav-link">Publications</a></li>
+                    <li><a :href="BASE_URL + 'awards_press.html'" class="tm-nav-link">Awards/Press</a></li>
+                    <li><a :href="BASE_URL + 'cv.html'" class="tm-nav-link">CV</a></li>
+                </ul>
+            </div>
         </div>
     </div>
     <div id="nav-spacer"></div>
 
-    <div class="off-canvas-wrapper">
+    <div class="off-canvas-wrapper hide-for-large">
         <div class="off-canvas position-left" id="tm-mobile-menu" data-transition="overlap" data-off-canvas>
             <div class="tm-menu-heading">
                 <p class="tm-menu-heading-text">Navigation</p>
@@ -47,7 +63,12 @@ let email = ref('tmarrinan@stthomas.edu');
     width: 100%;
     height: 3.5rem;
     background-color: #472568;
-    text-align: center;
+    text-align: left;
+}
+
+#tm-mobile-top-bar {
+    overflow: hidden;
+    position: static;
 }
 
 #tm-mobile-menu{
@@ -60,23 +81,33 @@ let email = ref('tmarrinan@stthomas.edu');
 }
 
 #tm-info {
-    float: left;
-    text-align: left;
+    position: absolute;
+    display: block;
+    left: 3rem;
     margin: 0.5rem 0 0 1.25rem;
-    width: 35.5rem;
     height: 2.5rem;
+    white-space: nowrap;
 }
 
-.tm-menu {
-    float: left;
+#tm-menu {
+    position: absolute;
     display: block;
+    left: 0rem;
     box-sizing: content-box;
     width: 2.5rem;
     padding: 0.25rem 0;
-    margin: .375rem 0 0 1.25rem;
+    margin: 0.375rem 0 0 0.5rem;
     border: 0.125rem solid #D1C9D9;
     border-radius: 0.5rem;
+    text-align: center;
     cursor: pointer;
+}
+
+#tm-navlinks{
+    position: absolute;
+    display: block;
+    left: 27.5rem;
+    top: 0.625rem;
 }
 
 .tm-menu-icon {
@@ -116,7 +147,7 @@ let email = ref('tmarrinan@stthomas.edu');
 }
 
 .tm-info-text {
-    font-size: 0.875rem;
+    font-size: 0.825rem;
     color: #D1C9D9;
     margin-bottom: 0;
     line-height: 1.5;
@@ -124,6 +155,17 @@ let email = ref('tmarrinan@stthomas.edu');
 
 .tm-bold {
     font-weight: bold;
+}
+
+.tm-nav-link, .tm-nav-link:visited {
+    font-size: 1rem;
+    font-style: normal;
+    color: #FFFFFF;
+}
+
+.tm-nav-link:hover {
+    text-decoration: underline;
+    color: #FFFFFF;
 }
 
 .tm-menu-link, .tm-menu-link:visited {
